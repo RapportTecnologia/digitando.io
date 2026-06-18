6907afeec3ca7ebaaa0dbd4629fa0c8c966e6720
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2026-06-17 21:46:51 -0300

    Adicionado o google AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1710447158146560"
+          crossOrigin="anonymous"
+        ></script>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
